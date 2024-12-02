0a4c427824a04bdfea311fe27436a3aff52feaa0
Daniel-1-2-3 <144050857+Daniel-1-2-3@users.noreply.github.com>
2024-12-02 10:30:34 -0500

    Initial commit, empty template

---
+++ MediWatch/src/App.jsx
@@ -0,0 +1,12 @@
+
+const App = () => {
+  return (
+    <div className="bg-slate-100 w-full h-screen flex flex-col">
+      <div className="bg-red-300 w-full h-1/4 text-center">
+        <text className="bg-blue-200 rounded-md">Placeholder Text</text>
+      </div>
+    </div>
+  )
+}
+
+export default App
